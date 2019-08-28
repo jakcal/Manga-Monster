da1fc1ecf321e502b62139554f2eda57162ee820
Henry Bowe <henrybowe@admins-Air-2.hsd1.ca.comcast.net>
2019-08-27 17:21:05 -0700

    html/css added for login/signup form on landing

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
 const app = express()
-const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-})
+app.use(express.static(path.join(__dirname, '/public')))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
+
+require('./routes/auth.js')(app)
+
+module.exports = app 
