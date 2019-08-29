82ff003f014d4ea2bbabe12744d72f2cda35b7bb
Henry <hleejr0103@gmail.com>
2019-08-29 09:21:51 -0700

    [add] user can sign up and login

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,45 @@ module.exports = (app) => {
         res.render('login');
     })
 
-    app.post('/sign-up', (req, res) => {
-        const user = new User(req.body);
+    app.post('/signup', (req, res) => {
+        const user = new User(req.body)
         user.save().then((user) => {
-            const token = jwt.sign({_id: user._id}, process.env.SECRET, {expiresIn: '60 days'});
-            res.cookie('nToken', token, { maxAge: 900000, httpOnly: true});
-            res.redirect(`/`);
+            const token = jwt.sign({_id: user._id}, process.env.SECRET, {expiresIn: '60 days'})
+            res.cookie('nToken', token, { maxAge: 900000, httpOnly: true})
+            res.redirect(`/`)
         }).catch(err => {
-            console.log(err.message);
+            console.log(err.message)
             return res.status(400).send({err:err})
         })
+    })
+
+    app.post('/login', (req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+        // Find this user name
+        User.findOne({ username }, "username password")
+          .then(user => {
+            if (!user) {
+              // User not found
+              return res.status(401).send({ message: "Wrong Username or Password" })
+            }
+            // Check the password
+            user.comparePassword(password, (err, isMatch) => {
+              if (!isMatch) {
+                // Password does not match
+                return res.status(401).send({ message: "Wrong Username or password" })
+              }
+              // Create a token
+              const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                expiresIn: "60 days"
+              })
+              // Set a cookie and redirect to root
+              res.cookie("nToken", token, { maxAge: 900000, httpOnly: true })
+              res.redirect(`/home/${user.username}`)
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
     })
 }
