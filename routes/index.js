527228f0e6cb72eb9b18383b6e9b18fa1daaef3a
Henry <hleejr0103@gmail.com>
2019-09-03 21:29:31 -0700

    [add] users can view manga on homepage

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const Manga = require("./../models/manga")
 module.exports = (app) => {
     app.get('/home', (req, res) => {
         Manga.find().then( manga => {
+            // const views = []
+            // for (i = 0; i < manga.length; i+=1) {
+            //     const view = Number(manga[i].link.split("\n")[3].split(" ")[1])
+            //     views.push(view)
+            // }
+            // console.log(views) 
             res.render('home', { data: manga});
         })
     })
