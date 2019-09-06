0356a91bf810b1d154b8b5eb13e5731e5b18d58a
Henry <hleejr0103@gmail.com>
2019-09-06 09:31:22 -0700

    [add] user can view comics from all demographics on homepage

--- models/manga.js
+++ models/manga.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MangaSchema = new Schema ({
-    "web-scraper-order": { type: String, text: true },
-    "web-scraper-start-url": { type: String, text: true },
-    link : { type: String, text: true },
+    num: { type: Number },
     "link-href": { type: String, text: true },
     title: { type: String, text: true },
     authors: { type: String, text: true },
@@ -12,7 +10,8 @@ const MangaSchema = new Schema ({
     genres: { type: String, text: true },
     demographic: { type: String, text: true },
     description: { type: String, text: true },
-    "image-src": { type: String, text: true },    
+    "image-src": { type: String, text: true },
+    views: { type: Number },    
 })
 
 module.exports = mongoose.model('Manga', MangaSchema)
